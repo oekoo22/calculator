cf37a6407bb39e993d288859cb87cb330fe62dd8
oekoo <y.dykow22@gmail.com>
2022-08-29 16:09:33 +0200

    Get access to the index buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const opertaionButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandButton = document.querySelector('[data-previous-operand]');
+const currentOperandButton = document.querySelector('[data-current-operand]');
