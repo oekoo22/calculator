328814f16e80f5aacde1441136d00e53e4344f4e
oekoo <y.dykow22@gmail.com>
2022-08-29 17:05:15 +0200

    Add eventListener for equalsButton

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ class Calculator {
 
     chooseOperation(operation) {
         if (this.currentOperand === '') return; 
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
         this.operation = operation;
         this.previousOperand = this.currentOperand;
         this.currentOperand = '';
@@ -63,3 +66,8 @@ operationButtons.forEach(button => {
         calculator.updateDisplay()
     })
 });
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
