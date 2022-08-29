5e48737ba5e90d1cb6d521a6842749ebc7e10560
oekoo <y.dykow22@gmail.com>
2022-08-29 16:16:46 +0200

    Draw the calculator-functions in a class

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+class Calculator {
+    constructor(previousOperandButton, currentOperandButton) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber() {
+
+    }
+
+    chooseOperation() {
+
+    }
+
+    compute() {
+        
+    }
+}
+
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]');
-const opertaionButtons = document.querySelectorAll('[data-operation]');
+const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandButton = document.querySelector('[data-previous-operand]');
 const currentOperandButton = document.querySelector('[data-current-operand]');
+
